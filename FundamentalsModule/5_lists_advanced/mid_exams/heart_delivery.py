b6f825cf1f7e90802317cb200587c1a496b53504
Dzhansu Zalim <djansuevil@gmail.com>
2023-10-15 17:12:47 +0300

    Update heart_delivery.py

--- FundamentalsModule/5_lists_advanced/mid_exams/heart_delivery.py
+++ FundamentalsModule/5_lists_advanced/mid_exams/heart_delivery.py
@@ -6,22 +6,15 @@ while command != "Love!":
     split_command = command.split()
     step_to_jump = int(split_command[1])
     index_to_jump = current_index_of_cupid + step_to_jump
-    if index_to_jump < len(size_of_neighborhood):
-        if size_of_neighborhood[index_to_jump] == 0:
-            print(f"Place {index_to_jump} already had Valentine's day.")
-        else:
-            size_of_neighborhood[index_to_jump] -= 2
-            if size_of_neighborhood[index_to_jump] == 0:
-                print(f"Place {index_to_jump} has Valentine's day.")
-        current_index_of_cupid = index_to_jump
+    if index_to_jump >= len(size_of_neighborhood):
+        index_to_jump = 0
+    if size_of_neighborhood[index_to_jump] == 0:
+        print(f"Place {index_to_jump} already had Valentine's day.")
     else:
-        current_index_of_cupid = 0
-        if size_of_neighborhood[current_index_of_cupid] == 0:
-            print(f"Place {current_index_of_cupid} already had Valentine's day.")
-        else:
-            size_of_neighborhood[current_index_of_cupid] -= 2
-            if size_of_neighborhood[current_index_of_cupid] == 0:
-                print(f"Place {current_index_of_cupid} has Valentine's day.")
+        size_of_neighborhood[index_to_jump] -= 2
+        if size_of_neighborhood[index_to_jump] == 0:
+            print(f"Place {index_to_jump} has Valentine's day.")
+    current_index_of_cupid = index_to_jump
     command = input()
 
 print(f"Cupid's last position was {current_index_of_cupid}.")
