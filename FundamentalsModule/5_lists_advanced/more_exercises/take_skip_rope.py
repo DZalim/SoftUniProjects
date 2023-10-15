c496c8659fba66fc8345b4c53602cb1bf01d399c
Dzhansu Zalim <djansuevil@gmail.com>
2023-10-15 14:42:04 +0300

    Create take_skip_rope.py

---
+++ FundamentalsModule/5_lists_advanced/more_exercises/take_skip_rope.py
@@ -0,0 +1,26 @@
+string = input()
+
+list_of_numbers = [int(digit) for digit in string if digit.isdigit()]
+list_of_strings = [letter for letter in string if not letter.isdigit()]
+
+take_list_of_numbers = [list_of_numbers[take] for take in range(len(list_of_numbers)) if take % 2 == 0]
+skip_list_of_numbers = [list_of_numbers[take] for take in range(len(list_of_numbers)) if take % 2 != 0]
+
+final_string = []
+list_index = 0
+while list_of_strings:
+    if len(list_of_strings) < take_list_of_numbers[list_index]:
+        take_list_of_numbers[list_index] = len(list_of_strings)
+    if take_list_of_numbers[list_index] > 0:
+        for append_in_final_string in range(take_list_of_numbers[list_index]):
+            final_string.append(list_of_strings[append_in_final_string])
+    if len(list_of_strings) < (skip_list_of_numbers[list_index] + take_list_of_numbers[list_index]):
+        skip_list_of_numbers[list_index] = len(list_of_strings)
+        take_list_of_numbers[list_index] = 0
+    for remove_string in range(take_list_of_numbers[list_index] + skip_list_of_numbers[list_index] - 1, -1, -1):
+        list_of_strings.remove(list_of_strings[remove_string])
+    list_index += 1
+    if list_index == len(take_list_of_numbers):
+        break
+
+print("".join(final_string))
