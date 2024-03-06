1864bc0448a1590ac31ebd241fac568acf1b35ab
Dzhansu Zalim <djansuevil@gmail.com>
2024-03-06 15:06:44 +0200

    Create profile.py

---
+++ AdvancedModule/OOP/4_encapsulation/lab/profile.py
@@ -0,0 +1,44 @@
+class Profile:
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if len(value) < 5 or len(value) > 15:
+            raise ValueError("The username must be between 5 and 15 characters.")
+
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        is_contain_characters = len(value) >= 8
+        is_contain_uppercase = len([char for char in value if char.isupper()]) > 0
+        is_contain_digit = len([char for char in value if char.isdigit()]) > 0
+
+        result = is_contain_characters and is_contain_uppercase and is_contain_digit
+
+        if not result:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
+
+
+#profile_with_invalid_password = Profile('My_username', 'My-password')
+
+# profile_with_invalid_username = Profile('Too_long_username', 'Any')
+
+correct_profile = Profile("Username", "Passw0rd")
+print(correct_profile)
