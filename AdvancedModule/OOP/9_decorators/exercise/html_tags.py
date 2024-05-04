67c768eb9d519f5a5aa4d952808f0e3733dfac3d
Dzhansu Zalim <djansuevil@gmail.com>
2024-05-04 13:38:56 +0300

    create decorators exercises

---
+++ AdvancedModule/OOP/9_decorators/exercise/html_tags.py
@@ -0,0 +1,26 @@
+def tags(tag: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return f"<{tag}>{func(*args, **kwargs)}</{tag}>"
+
+        return wrapper
+
+    return decorator
+
+
+@tags('body')
+@tags('div')
+@tags('p')
+def say_hi():
+    return "Hello"
+
+
+print(say_hi())
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+print(join_strings("Hello", " you!"))
